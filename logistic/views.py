1150a58b830c6c2bc7eb0570f0f5e873f5fb732f
Kushnirchuk Maxim <maximivanovgaz@gmail.com>
2024-01-19 11:18:06 +0300

    not end

--- logistic/views.py
+++ logistic/views.py
@@ -24,4 +24,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'ALL WORKING!!!   WINNN!!!!!!!!'})
+    return Response({'message': 'not   WINNN!!!!!!!!'})
